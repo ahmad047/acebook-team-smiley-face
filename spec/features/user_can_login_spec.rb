ce7caa17874d445bdd7263c4e4aed4d719bd71dc
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-18 17:18:50 +0100

    Login feature test passing

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 feature "Login" do
   scenario "Can login with existing credentials" do
-    visit "/"
+    visit "/login"
     fill_in "Email", with: "user@mail.com"
     fill_in "Password", with: "password123"
-    click_button "Save changes"
+    click_button "Login"
     expect(page.status_code).to be(200)
-    expect(page).to have_current_path("/login")
-    save_and_open_page
+    expect(page).to have_current_path("/")
     expect(page).not_to have_content("Login")
   end
 end

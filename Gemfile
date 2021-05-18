3d1a21cc6a8f558d23125da87f74717d722026bc
YohannTisserand <tisserand.yohann@gmail.com>
2021-05-18 15:02:31 +0100

    trying to redirect the submit button signup

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rspec-rails', '~> 4.0'
+  gem 'launchy'
 end
 
 group :development do
---
+++ spec/features/user_can_signup_and_be_redirected_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario 'a user can sing up and be redirected' do
+    visit '/users/new'
+    fill_in 'user[first_name]', with: 'john'
+    fill_in 'user[last_name]', with: 'doe'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'password123'
+    fill_in 'user[password_confirmation]', with: 'password123'
+    click_button 'Create User'
+    expect(current_path).to eq(root_path)
+  end
+end
+
+
+# visit '/sessions'
+# fill_in(:username, with: 'johndoe')
+# fill_in(:password, with: 'password123')
+# click_button('sign in')

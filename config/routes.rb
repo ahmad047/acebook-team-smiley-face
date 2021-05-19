aa479091c2c2a1e1caaab10c42ddafd19350fba7
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-19 14:34:38 +0100

    Index render working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login',to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :users
   root 'posts#index'

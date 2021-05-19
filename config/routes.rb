80f6ab10b3eecb868f47a64b0ac6efc21b272028
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-19 15:58:10 +0100

    fixed the routing of destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  root "posts#index"
+
   get '/login', to: 'sessions#new'
   post '/login',to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
 
+
+
   resources :users
-  root 'posts#index'
+
+  delete '/users', to: 'users#destroy'
+
 
   resources :posts
 end

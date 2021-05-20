66230dc6a757dc8faff0a2b5ce2daf192b3a4bc5
“Phil <philsmithies@gmail.com>
2021-05-20 12:04:39 +0200

    users/users removed, navbar updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/login',to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
   get '/logout',to: 'sessions#destroy'
+  get '/users/:id', to: 'users#show'
 
   resources :users
   root 'posts#index'

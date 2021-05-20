24fba91f510676798d0f89ed4860f15673de3234
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-20 15:51:33 +0100

    Fixed log out test

--- spec/features/user_can_sign_out_spec.rb
+++ spec/features/user_can_sign_out_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-feature 'A user can sign out' do
-  scenario 'signing out' do
-    visit '/login'
-    fill_in 'Email', with: 'jason@him.com'
-    fill_in 'Password', with: '123456'
-    click_button 'Login'
-    click_button 'Log Out'
+feature "A user can log out" do
+  scenario "logging out" do
+    visit "/login"
+    fill_in "Email", with: "bobross@mail.com"
+    fill_in "Password", with: "password123"
+    click_button "Login"
+    click_link "Log Out"
     expect(current_path).to eq(root_path)
   end
 end

8afd5d68848ab9ba53dbecad05b85596dea3a2d3
Phil Smithies <60888215+phileeep@users.noreply.github.com>
2021-05-20 12:56:15 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login',to: 'sessions#create'
   delete '/logout',to: 'sessions#destroy'
+  get '/logout',to: 'sessions#destroy'
+  get '/users/:id', to: 'users#show'
 
   resources :users
   root 'posts#index'
+  # login 'sessions#new'
 
   resources :posts
 end

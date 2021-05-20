8775a5d6da2feab9da0d74703e030bc9841cfb69
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-20 14:08:06 +0100

    Bugs fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       redirect_to root_url, notice: "Signed up successfully"
     else
       render :new

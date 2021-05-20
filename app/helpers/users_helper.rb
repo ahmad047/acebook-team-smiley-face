89f715a54e786a7a6737857f9985ebcf8cf6e543
YohannTisserand <tisserand.yohann@gmail.com>
2021-05-20 18:04:18 +0100

    a user is can upload a profile picture but sign up needs to be debugged

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,2 @@
 module UsersHelper
-  # def pfp_for(user)
-  #   pfp_id = Digest::MD5::hexdigest(user.email.downcase)
-  #   pfp_url = "https://www.vippng.com/png/detail/205-2053434_bobross-bob-ross-painting-painter-freetoedit-bob-ross.png"
-  #   image_tag(pfp_url, alt: user.first_name, class: "pfp")
-  #   end
 end

b66b826eef9258c5a5c693cb0aaa252677c86e07
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-20 15:39:20 +0100

    Fixed test for users submitting posts and viewing them

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
-# RSpec.feature "Timeline", type: :feature do
 feature "Timeline" do
-  scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
-    expect(page).to have_content("Posted less than a minute ago")
+  scenario "Can submit posts, view them and see how long ago they were posted" do
+    visit '/users/new'
+    fill_in 'user[first_name]', with: 'Richard'
+    fill_in 'user[last_name]', with: 'McMahon'
+    fill_in 'user[email]', with: 'rich@gmail.com'
+    fill_in 'user[password]', with: '1234'
+    fill_in 'user[password_confirmation]', with: '1234'
+    click_button 'Create User'
+    fill_in 'post[message]', with: 'Hello, world!'
+    click_button 'Submit'
+    expect(page).to have_content('Hello, world!')
+    expect(page).to have_content('Posted less than a minute ago')
   end
 end

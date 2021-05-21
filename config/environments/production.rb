88e7a209ff95c83d3974b7530091c80e4f2a31ae
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-21 16:17:42 +0100

    Adjusting production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -46,11 +46,11 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]

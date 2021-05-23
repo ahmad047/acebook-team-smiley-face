d53da3e01f7e2ca5077d1253cc22bb8a62b19682
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-23 19:23:34 +0100

    fixed sessions tests

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe "Sessions", type: :request do
 
   describe "GET /destroy" do
     it "returns http success" do
+      
       get "/sessions/destroy"
       expect(response).to have_http_status(:success)
     end

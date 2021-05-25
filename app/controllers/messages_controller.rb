9eab9968e86cda87bc3d38ca8622352f29943da2
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-25 20:34:28 +0100

    Halfway there Action Cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
+ 
   def create 
-    message = Message.create(sender_id: params[:sender_id], receiver_id: params[:receiver_id], text: params[:message])
-    redirect_back(fallback_location: "/users")
-  end
+      message = Message.create(sender_id: params[:sender_id], receiver_id: params[:receiver_id], text: params[:message])
+      SendMessageJob.perform_later(message)
+  end 
 end

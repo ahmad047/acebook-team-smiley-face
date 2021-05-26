f392d3cd03525d1a7fad546b8af37d36b06f3b3f
YohannTisserand <tisserand.yohann@gmail.com>
2021-05-26 19:14:01 +0100

    a user can get an email to confirm the registration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def registration_confirmation(user)
     @user = user
-    mail(to: @user.email, content_type: "text/html", subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, content_type: "text/html", subject: 'Welcome to Acebook')
  end
 end

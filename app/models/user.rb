e3958f32beb0b59f74b3c8da9821f16ab4f5ad36
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-05-26 11:06:46 +0100

    Email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :posts
-  has_secure_password
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_one_attached :avatar
   has_many :likes, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, length: { maximum: 280 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  has_secure_password
+  validates :password, length: { minimum: 10 }
+
+  
 end

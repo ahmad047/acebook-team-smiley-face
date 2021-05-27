6baed5549621bc3f7647efda329c27b60fea5f73
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-27 20:31:42 +0100

    Second last test passing, host set up in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,17 @@ Rails.application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    user_name: 'bobrossblocks@gmail.com',
+    password: 'zjbcpvieoeayeooa',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }  
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that

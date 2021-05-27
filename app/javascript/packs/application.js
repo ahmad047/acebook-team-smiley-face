20cc8fa15ca3d40e383ba8a544f99e9885c83a6b
Adam <adam.j.mccor@gmail.com>
2021-05-27 02:31:08 +0100

    'added package.json dependencies'

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,13 @@ require("channels")
 import "./stylesheets/application.scss";
 import "bootstrap";
 import "./application.scss";
+require('popper.js');
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
